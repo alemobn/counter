ba2cf658867f15571c24c1d5067450b4b6f90b0f
alemobn <alemoura.zyc@gmail.com>
2024-11-14 17:40:16 -0300

    feat: add script

--- counter.js
+++ counter.js
@@ -1,3 +1,29 @@
-export function setupCounter(element) {
-  
+export function setupCounter() {
+  const { counter, btnIncrease, btnReset, btnDecrease } = {
+    counter: document.querySelector('.counter'),
+    btnIncrease: document.querySelector('.increase'),
+    btnReset: document.querySelector('.reset'),
+    btnDecrease: document.querySelector('.decrease')
+  };
+
+  let count = 0;
+
+  counter.textContent = count;
+
+  btnIncrease.addEventListener('click', () => {
+    count++
+    counter.textContent = count;
+  });
+
+  btnReset.addEventListener('click', () => {
+    count = 0;
+    counter.textContent = count;
+  });
+
+  btnDecrease.addEventListener('click', () => {
+    if (count > 0) {
+      count--;
+      counter.textContent = count;
+    }
+  });
 }
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <div class="container">
     <h1>Counter</h1>
-    <span class="counter">0</span>
+    <span class="counter"></span>
     <section class="btns">
       <button class="decrease">decrease</button>
       <button class="reset">reset</button>
@@ -13,4 +13,4 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter()
